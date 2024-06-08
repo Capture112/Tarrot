3447ed8a13e58349556afee3ca12ddddf78b08ff
Capture112 <nmosk16@gmail.com>
2024-06-08 17:59:18 +0300

    updated

--- main.py
+++ main.py
@@ -1,43 +1,5 @@
-import random
-
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from random import SystemRandom
-import os
-
-sys_rand = SystemRandom()
-app = FastAPI()
-
-#Директории
-tarrot_dir = os.listdir('Tarrot')
-tarrot_major_dir = os.listdir('Tarrot_Major')
-items = [1,2,3,4,5]
-
-@app.get('/')
-def root():
-    return {'message': "HI"}
-
-@app.get('/items')
-def get_item(item_id: int):
-    try:
-        return images[item_id -1]
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='item does not exists')
+import uvicorn
 
 
-#Tarrot
-@app.get('/rand',response_class = FileResponse)
-def get_random():
-    try:
-        rand_image = tarrot_dir[random.randrange(1, 10)]
-        return ("Tarrot/" + rand_image)
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='item does not exists')
-
-@app.get('/rand_major',response_class = FileResponse)
-def get_random_major():
-    try:
-        rand_image = tarrot_major_dir[random.randrange(1, 10)]
-        return ("Tarrot_Major/" + rand_image)
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='item does not exists')
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="127.0.0.1", port=8000, reload=True)
